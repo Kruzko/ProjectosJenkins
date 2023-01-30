23a8da0c658ab0dbe4e2de879c8c5f76f05bcae7
Juan Jose Marin Molina <Hermanomayor2@gmail.com>
2023-01-30 14:21:32 +0100

    Commit message

---
+++ Ptienda/dslTienda.groovy
@@ -0,0 +1,20 @@
+// import jenkins-auto-approve-script.groovy
+
+job('Job-pTienda') {
+    description('Job Padre para crear una tarea Hija que despliegue la App Tienda del Corte Ingles')
+    scm {
+        git('https://github.com/Kruzko/ProyectoTienda', 'master'){ 
+            node -> 
+                node / gitConfigName('Kruzko')
+                node / gitConfigEmail('hermanomayor2@gmail.com')
+        }
+    }
+    triggers {
+     //   scm('H/5 * * * *')
+        githubPush()
+    }
+    
+    steps {
+        shell("bash install.sh")
+    }
+}
